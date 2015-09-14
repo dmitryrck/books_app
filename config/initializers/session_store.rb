a9abb2492cb02441d4ba95c6b34f312b0f5c0faa
Dmitry Rocha <dmitryrck@gmail.com>
2015-09-14 17:19:40 -0300

    Rename app to BooksApp.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_leitura_thc_app_session'
+Rails.application.config.session_store :cookie_store, key: '_books_app_session'
