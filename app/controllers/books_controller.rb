5b0cd5ab3c0f936dc85effa823cc2d110955b5f7
Dmitry Rocha <dmitryrck@gmail.com>
2015-09-20 07:34:02 -0300

    Order books at home page.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books = Book.ordered
   end
 
   def new
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ActiveRecord::Base
   validates :title, :isbn, :authors, presence: true
 
+  scope :ordered, -> { order(:title) }
+
   def to_s
     title
   end
