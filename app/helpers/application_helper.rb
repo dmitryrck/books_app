f8b2eff75ea04d433347939e02856c4697f762ed
Dmitry Rocha <dmitryrck@gmail.com>
2015-09-24 21:26:55 -0300

    Fix `book_image_tag`:
    Rename to `book_image_url` and use `image_tag` with it.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def book_image_tag(book)
+  def book_image_url(book)
     return 'http://placehold.it/115x180.png' if book.image_link.blank?
 
     book.image_link
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
-  context '#book_image_tag' do
+  context '#book_image_url' do
     it 'return placehold image if nil image link book' do
       book = double(:Book, image_link: nil)
-      expect(helper.book_image_tag(book)).to eq 'http://placehold.it/115x180.png'
+      expect(helper.book_image_url(book)).to eq 'http://placehold.it/115x180.png'
     end
 
     it 'return placehold image if empty string image link book' do
       book = double(:Book, image_link: '')
-      expect(helper.book_image_tag(book)).to eq 'http://placehold.it/115x180.png'
+      expect(helper.book_image_url(book)).to eq 'http://placehold.it/115x180.png'
     end
 
     it 'return image link image if book has a imagem link' do
       book = double(:Book, image_link: 'http://example.com/1.jpg')
-      expect(helper.book_image_tag(book)).to eq 'http://example.com/1.jpg'
+      expect(helper.book_image_url(book)).to eq 'http://example.com/1.jpg'
     end
   end
 end
