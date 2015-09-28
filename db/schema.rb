91c676fa688ce9e4a8c557d43bc7f950b0f4d772
Dmitry Rocha <dmitryrck@gmail.com>
2015-09-28 18:29:04 -0300

    Book isbn must be uniq.
    Closes #13

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925002856) do
+ActiveRecord::Schema.define(version: 20150928212522) do
 
   create_table "books", force: :cascade do |t|
     t.string  "title"
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20150925002856) do
     t.string  "image_link"
     t.integer "quantity",       default: 0
   end
+
+  add_index "books", ["isbn"], name: "index_books_on_isbn", unique: true
 
 end
